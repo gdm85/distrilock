8a88ef7bb248896bf685938faa042c05563ddcb4
gdm85 <gdm85@users.noreply.github.com>
2017-05-23 20:48:34 +0200

    Adding stub for TCP server

---
+++ cli/distrilock/example.go
@@ -0,0 +1,115 @@
+// This package contains the command line interface executable 'distrilock'.
+// To read its command line help, run:
+/* $ bin/distrilock --help */
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"syscall"
+	"time"
+)
+
+func exampleMain() {
+	var err error
+	var f *os.File
+	
+	what := os.Args[1]
+	if what == "write" {
+		err = writeLockSet("book.dat")
+	} else if what == "read" {
+		err = readLockSet("book.dat")
+	} else if what == "acquire" {
+		f, err = acquireLock("book.dat")
+	} else {
+		err = fmt.Errorf("invalid command")
+	}
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	
+	if what == "acquire" {
+		fmt.Println("waiting 3 seconds before release")
+		time.Sleep(3 * time.Second)
+		err = releaseLock(f)
+	}
+	
+	
+	fmt.Println("Press Enter to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n') 
+}
+
+func acquireLock(fname string) (*os.File, error) {
+	fi, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE, 0664)
+	if err != nil {
+		return nil, err
+	}
+	fd := fi.Fd()
+
+	var lt syscall.Flock_t
+	lt.Type = syscall.F_WRLCK
+	lt.Whence = int16(os.SEEK_SET)
+	saveLock := lt // make a copy
+
+	return fi, syscall.FcntlFlock(fd, syscall.F_SETLK, &saveLock)
+}
+
+func releaseLock(f *os.File) error {
+	fd := f.Fd()
+
+	var lt syscall.Flock_t
+	lt.Type = syscall.F_UNLCK
+	lt.Whence = int16(os.SEEK_SET)
+
+	return syscall.FcntlFlock(fd, syscall.F_SETLK, &lt)
+}
+
+func writeLockSet(fname string) error {
+	fi, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE, 0664)
+	if err != nil {
+		return err
+	}
+	fd := fi.Fd()
+
+	var lt syscall.Flock_t
+	lt.Type = syscall.F_WRLCK
+	lt.Whence = int16(os.SEEK_SET)
+	saveLock := lt // make a copy
+
+	err = syscall.FcntlFlock(fd, syscall.F_GETLK, &lt)
+	if err != nil {
+		return err
+	}
+	if lt.Type == syscall.F_WRLCK {
+		return fmt.Errorf("Process %v has a write lock already!", lt.Pid)
+	} else if lt.Type == syscall.F_RDLCK {
+		return fmt.Errorf("Process %v has a read lock already!", lt.Pid)
+	}
+	return syscall.FcntlFlock(fd, syscall.F_SETLK, &saveLock)
+}
+
+func readLockSet(fname string) error {
+	fi, err := os.OpenFile(fname, os.O_RDONLY, 0664)
+	if err != nil {
+		return err
+	}
+	fd := fi.Fd()
+
+	var lt syscall.Flock_t
+	lt.Type = syscall.F_RDLCK
+	lt.Whence = int16(os.SEEK_SET)
+	lt.Len = 50
+	saveLock := lt // make a copy
+
+	err = syscall.FcntlFlock(fd, syscall.F_GETLK, &lt)
+	if err != nil {
+		return err
+	}
+	if lt.Type == syscall.F_WRLCK {
+		return fmt.Errorf("Process %v has a write lock already!", lt.Pid)
+	}
+
+	return syscall.FcntlFlock(fd, syscall.F_SETLK, &saveLock)
+}
--- cli/distrilock/main.go
+++ cli/distrilock/main.go
@@ -1,115 +1,76 @@
-// This package contains the command line interface executable 'distrilock'.
-// To read its command line help, run:
-/* $ bin/distrilock --help */
 package main
 
 import (
-	"fmt"
-	"os"
-	"bufio"
-	"syscall"
-	"time"
+    "fmt"
+    "net"
+    "io"
+    "time"
+    "os"
+)
+
+const (
+    CONN_HOST = "localhost"
+    CONN_PORT = "8071"
 )
 
 func main() {
-	var err error
-	var f *os.File
+	a, err := net.ResolveTCPAddr("tcp", CONN_HOST+":"+CONN_PORT)
+    if err != nil {
+        fmt.Println("Error resolving:", err.Error())
+        os.Exit(1)
+    }
 	
-	what := os.Args[1]
-	if what == "write" {
-		err = writeLockSet("book.dat")
-	} else if what == "read" {
-		err = readLockSet("book.dat")
-	} else if what == "acquire" {
-		f, err = acquireLock("book.dat")
-	} else {
-		err = fmt.Errorf("invalid command")
-	}
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	
-	if what == "acquire" {
-		fmt.Println("waiting 3 seconds before release")
-		time.Sleep(3 * time.Second)
-		err = releaseLock(f)
-	}
-	
-	
-	fmt.Println("Press Enter to continue...")
-	bufio.NewReader(os.Stdin).ReadBytes('\n') 
+    // Listen for incoming connections.
+    l, err := net.ListenTCP("tcp", a)
+    if err != nil {
+        fmt.Println("Error listening:", err.Error())
+        os.Exit(1)
+    }
+    // Close the listener when the application closes.
+    defer l.Close()
+    fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+    for {
+        // Listen for an incoming connection.
+        conn, err := l.AcceptTCP()
+        if err != nil {
+            fmt.Println("Error accepting: ", err.Error())
+            os.Exit(1)
+        }
+        // Handle connections in a new goroutine.
+        go handleRequest(conn)
+    }
 }
 
-func acquireLock(fname string) (*os.File, error) {
-	fi, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE, 0664)
+// Handles incoming requests.
+func handleRequest(conn *net.TCPConn) {
+	// setup keep-alive
+	err := conn.SetKeepAlive(true)
 	if err != nil {
-		return nil, err
+		panic(err.Error())
 	}
-	fd := fi.Fd()
-
-	var lt syscall.Flock_t
-	lt.Type = syscall.F_WRLCK
-	lt.Whence = int16(os.SEEK_SET)
-	saveLock := lt // make a copy
-
-	return fi, syscall.FcntlFlock(fd, syscall.F_SETLK, &saveLock)
-}
-
-func releaseLock(f *os.File) error {
-	fd := f.Fd()
-
-	var lt syscall.Flock_t
-	lt.Type = syscall.F_UNLCK
-	lt.Whence = int16(os.SEEK_SET)
-
-	return syscall.FcntlFlock(fd, syscall.F_SETLK, &lt)
-}
-
-func writeLockSet(fname string) error {
-	fi, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE, 0664)
+	err = conn.SetKeepAlivePeriod(time.Second*3)
 	if err != nil {
-		return err
+		panic(err.Error())
 	}
-	fd := fi.Fd()
-
-	var lt syscall.Flock_t
-	lt.Type = syscall.F_WRLCK
-	lt.Whence = int16(os.SEEK_SET)
-	saveLock := lt // make a copy
-
-	err = syscall.FcntlFlock(fd, syscall.F_GETLK, &lt)
-	if err != nil {
-		return err
+	fmt.Println("a client connected")
+  // Make a buffer to hold incoming data.
+  buf := make([]byte, 1024)
+  for {
+	// Read the incoming connection into the buffer.
+	  reqLen, err := conn.Read(buf)
+	  if err != nil {
+		if err == io.EOF {
+			// other end interrupted connection
+			break
+		}
+		fmt.Println("Error reading:", err.Error())
+		continue
+	  }
+	  fmt.Println("received:", string(buf[:reqLen]))
+	  // Send a response back to person contacting us.
+	  conn.Write([]byte("Message received."))
 	}
-	if lt.Type == syscall.F_WRLCK {
-		return fmt.Errorf("Process %v has a write lock already!", lt.Pid)
-	} else if lt.Type == syscall.F_RDLCK {
-		return fmt.Errorf("Process %v has a read lock already!", lt.Pid)
-	}
-	return syscall.FcntlFlock(fd, syscall.F_SETLK, &saveLock)
-}
-
-func readLockSet(fname string) error {
-	fi, err := os.OpenFile(fname, os.O_RDONLY, 0664)
-	if err != nil {
-		return err
-	}
-	fd := fi.Fd()
-
-	var lt syscall.Flock_t
-	lt.Type = syscall.F_RDLCK
-	lt.Whence = int16(os.SEEK_SET)
-	lt.Len = 50
-	saveLock := lt // make a copy
-
-	err = syscall.FcntlFlock(fd, syscall.F_GETLK, &lt)
-	if err != nil {
-		return err
-	}
-	if lt.Type == syscall.F_WRLCK {
-		return fmt.Errorf("Process %v has a write lock already!", lt.Pid)
-	}
-
-	return syscall.FcntlFlock(fd, syscall.F_SETLK, &saveLock)
+  // Close the connection when you're done with it.
+  conn.Close()
+  fmt.Println("a client disconnected")
 }
